af9e0103410c122a9afa0449a2d5bf535012e989
Nathan Merry <nathanmerry9713@gmail.com>
2026-04-14 07:21:04 +0100

    fix ssl

--- pennywise-backend/src/lib/prisma.ts
+++ pennywise-backend/src/lib/prisma.ts
@@ -3,6 +3,10 @@ import { PrismaPg } from "@prisma/adapter-pg";
 
 const adapter = new PrismaPg({
   connectionString: process.env.DATABASE_URL!,
+  ssl:
+    process.env.DATABASE_SSL_REJECT_UNAUTHORIZED === "false"
+      ? { rejectUnauthorized: false }
+      : undefined,
 });
 
 export const prisma = new PrismaClient({ adapter });
